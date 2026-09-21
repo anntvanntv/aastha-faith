833f7d9e19a360de2e87131407b1df2b2b9995f4
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2026-09-21 16:46:32 +0545

    Use site pdf.png icon beside card title; keep icon on first text line

--- site/templates/publications.php
+++ site/templates/publications.php
@@ -23,7 +23,7 @@
                     <img class="cover" src="<?= $card->cover_image->url ?>" alt="<?= $card->title ?>">
                 <?php endif; ?>
                 <div class="publication-info">
-                    <h4><svg class="doc-icon" viewBox="0 0 24 24"><path d="M14 3H7a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8z"/><polyline points="14 3 14 8 19 8"/></svg><?= $card->title ?></h4>
+                    <h4><img class="doc-icon" src="<?= $config->urls->templates ?>icons/pdf.png" alt="icon"><?= $card->title ?></h4>
                 </div>
                 <a class="publication-download" href="<?= $card->pdf_file->url ?>" download>
                     <svg class="dl-icon" viewBox="0 0 24 24"><line x1="12" y1="4" x2="12" y2="15"/><polyline points="7 11 12 16 17 11"/><line x1="5" y1="20" x2="19" y2="20"/></svg>Click to Download
@@ -42,7 +42,7 @@
                     <img class="cover" src="<?= $card->cover_image->url ?>" alt="<?= $card->title ?>">
                 <?php endif; ?>
                 <div class="publication-info">
-                    <h4><svg class="doc-icon" viewBox="0 0 24 24"><path d="M14 3H7a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8z"/><polyline points="14 3 14 8 19 8"/></svg><?= $card->title ?></h4>
+                    <h4><img class="doc-icon" src="<?= $config->urls->templates ?>icons/pdf.png" alt="icon"><?= $card->title ?></h4>
                 </div>
                 <a class="publication-download" href="<?= $card->pdf_file->url ?>" download>
                     <svg class="dl-icon" viewBox="0 0 24 24"><line x1="12" y1="4" x2="12" y2="15"/><polyline points="7 11 12 16 17 11"/><line x1="5" y1="20" x2="19" y2="20"/></svg>Click to Download
