dffa3eefac9942077838bc3b6107df242507be58
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2026-09-23 13:05:44 +0545

    Fix strip_tags(null) deprecation on our-work cards

--- site/templates/our-work.php
+++ site/templates/our-work.php
@@ -71,7 +71,7 @@ namespace ProcessWire;
             <?php foreach ($page->children() as $work): ?>
                 <div class="our-work-card">
                     <h3><?= $work->title ?></h3>
-                    <p>Objective: <?= substr(strip_tags($work->objective), 0, 180) ?></p>
+                    <p>Objective: <?= substr(strip_tags($work->objective ?? ''), 0, 180) ?></p>
                     <a class="btn ow-btn" href="<?= $work->url ?>">Read more  <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M5.33333 0L4.39333 0.94L8.11333 4.66667H0V6H8.11333L4.39333 9.72667L5.33333 10.6667L10.6667 5.33333L5.33333 0Z"
